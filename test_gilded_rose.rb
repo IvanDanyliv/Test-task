ddc95161ff6f68dbb95ef3f77a16dc2e7cd71749
Ivan Danyliv <31760860+IvanDanyliv@users.noreply.github.com>
2025-03-11 00:34:13 +0200

    Updated tests to match corrected item quality update logic

--- test_gilded_rose.rb
+++ test_gilded_rose.rb
@@ -1,70 +1,52 @@
 require 'minitest/autorun'
 require_relative 'gilded_rose'
 
-class TestGildedRose < Minitest::Test
-  def setup
-    @items = [
-      Item.new("Aged Brie", 2, 0),
-      Item.new("Elixir", 5, 7),
-      Item.new("Sulfuras", 0, 80),
-      Item.new("Backstage passes", 15, 20),
-      Item.new("Conjured", 3, 6)
-    ]
-    @gilded_rose = GildedRose.new(@items)
+class GildedRoseTest < Minitest::Test
+  def test_aged_brie_increases_in_quality
+    items = [Item.new("Aged Brie", 10, 20)]
+    GildedRose.new(items).update_quality
+    assert_equal 21, items[0].quality
   end
 
-  def test_aged_brie
-    @gilded_rose.update_quality()
-    assert_equal 1, @items[0].quality
-    assert_equal 1, @items[0].sell_in
+  def test_backstage_passes_increase_in_quality
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 20)]
+    GildedRose.new(items).update_quality
+    assert_equal 21, items[0].quality
   end
 
-  def test_backstage_passes
-    item = Item.new("Backstage passes", 5, 45)
-    gilded_rose = GildedRose.new([item])
-
-    gilded_rose.update_quality()
-    assert_equal 48, item.quality
-    assert_equal 4, item.sell_in
-
-    item.sell_in = 0
-    gilded_rose.update_quality()
-    assert_equal 0, item.quality
+  def test_backstage_passes_increase_by_2_when_10_days_or_less
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+    GildedRose.new(items).update_quality
+    assert_equal 22, items[0].quality
   end
 
-  def test_sulfuras
-    @gilded_rose.update_quality()
-    assert_equal 80, @items[2].quality
-    assert_equal 0, @items[2].sell_in
+  def test_backstage_passes_increase_by_3_when_5_days_or_less
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 20)]
+    GildedRose.new(items).update_quality
+    assert_equal 23, items[0].quality
   end
 
-  def test_conjured_items
-    item = Item.new("Conjured", 3, 6)
-    gilded_rose = GildedRose.new([item])
-
-    gilded_rose.update_quality()
-    assert_equal 4, item.quality
-
-    gilded_rose.update_quality()
-    assert_equal 2, item.quality
-
-    gilded_rose.update_quality()
-    assert_equal 0, item.quality
+  def test_backstage_passes_drop_to_0_after_concert
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+    GildedRose.new(items).update_quality
+    assert_equal 0, items[0].quality
   end
 
-  def test_quality_never_exceeds_50
-    item = Item.new("Aged Brie", 10, 49)
-    gilded_rose = GildedRose.new([item])
-
-    gilded_rose.update_quality()
-    assert_equal 50, item.quality
+  def test_sulfuras_does_not_change
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 80)]
+    GildedRose.new(items).update_quality
+    assert_equal 80, items[0].quality
   end
 
-  def test_normal_item_quality_never_negative
-    item = Item.new("Normal Item", 5, 0)
-    gilded_rose = GildedRose.new([item])
+  def test_conjured_items_degrade_twice_as_fast
+    items = [Item.new("Conjured Mana Cake", 3, 6)]
+    GildedRose.new(items).update_quality
+    assert_equal 4, items[0].quality
+  end
 
-    3.times { gilded_rose.update_quality() }
-    assert_equal 0, item.quality
+  def test_conjured_items_degrade_twice_as_fast_after_sell_in
+    items = [Item.new("Conjured Mana Cake", 0, 6)]
+    GildedRose.new(items).update_quality
+    assert_equal 2, items[0].quality
   end
 end
